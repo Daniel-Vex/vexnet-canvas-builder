a1de207560bd69bbf08463eed52d2b3b20c8a62c
Daniel-Vex <danielpaiva.vex@gmail.com>
2025-07-28 09:48:15 -0300

    Congigurações do Vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // ADICIONE ESTA LINHA:
+  base: "/vexnet-canvas-builder/", // <--- MUITO IMPORTANTE!
+
   server: {
     host: "::",
     port: 8080,
